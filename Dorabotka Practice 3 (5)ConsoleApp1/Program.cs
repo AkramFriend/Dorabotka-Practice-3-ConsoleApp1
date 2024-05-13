90cc5b6baf034aec072b6c4cd33d9ab1bd78b378
shoevakramchik@gmail.com <shoevakramchik@gmail.com>
2024-05-14 02:21:35 +0300

    Добавьте файлы проекта.

---
+++ Dorabotka Practice 3 (5)ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorabotka_Practice_3__5_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Напишите максимальное целое число диапазона: ");
+            int n = int.Parse(Console.ReadLine());
+            int x = r.Next(0, n);
+            int count = 0;
+            for (; ; )
+            {
+                Console.WriteLine("Отгадайте загаданное число");
+                string input = Console.ReadLine();
+                count++;
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine($"Загаданным числом было: {x}");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    int userNumber = int.Parse(input);
+                    if (userNumber < x)
+                    {
+                        Console.WriteLine("Число меньше загаданного. Возьмите побольше.");
+                    }
+                    else if (userNumber > x)
+                    {
+                        Console.WriteLine("Число больше заагаданного. Возьмите поменьше.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Вы угадали, это {x}");
+                        Console.ReadKey();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Dorabotka Practice 3 ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorabotka_Practice_3_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                bool firstNumber = false;
+                Console.WriteLine("Напишите число на простоту: ");
+                string userInput = Console.ReadLine();
+                int num = Convert.ToInt32(userInput);
+                int divider = 2;
+                while (divider <= num - 1)
+                {
+                    int remains = num % divider;
+                    if (remains == 0)
+                    {
+                        firstNumber = true;
+                        break;
+                    }
+
+                    divider++;
+                }
+                if (firstNumber)
+                {
+                    Console.WriteLine("Составное");
+                }
+                else
+                {
+                    Console.WriteLine("Простое");
+                }                
+            }
+            Console.ReadKey();
+        }
+    }
+}
